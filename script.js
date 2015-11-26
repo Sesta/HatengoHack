7fe0a58aebae153f36004046d1d19563d3c4f95b
Sesta <sesta.ec5@gmail.com>
2015-11-27 00:53:09 +0900

    ajaxなどを動くようにしました

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 $(function(){
-  $( ".vcard-fullname" )
-    .css( "color", "red" );
+  $.ajax( {
+    type: "GET",
+    url: "http://210.140.71.3:8080/hello",
+    dataType: "json",
+    success: function( json ){
+      console.log( json );
+      alert( json.sample.message );
+    }
+  } )
 });
