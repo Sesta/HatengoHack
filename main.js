1896fb14f475c222ca6fd2cc3a01fd146cc5cc86
Sesta <sesta.ec5@gmail.com>
2015-11-28 18:20:39 +0900

    クリックできないバグを修正しました

--- main.js
+++ main.js
@@ -19,7 +19,8 @@ $(function(){
     "top": "0",
     "left": "0",
     "height": "100%",
-    "width": "100%"
+    "width": "100%",
+    "pointer-events": "none"
   } ) );
 
   $( ".entry-unit[data-eid]")
